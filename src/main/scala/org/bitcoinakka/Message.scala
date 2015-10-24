82d4e0dd85e7cfb013811b8dddbb7e1e9fa883ee
Hanh <hanh425@gmail.com>
2015-10-24 15:33:41 +0800

    Add helper functions for reading/writing primitive types

--- src/main/scala/org/bitcoinakka/Message.scala
+++ src/main/scala/org/bitcoinakka/Message.scala
@@ -3,7 +3,7 @@ package org.bitcoinakka
 import java.nio.ByteOrder
 import java.security.MessageDigest
 
-import akka.util.{ByteStringBuilder, ByteString}
+import akka.util.{ByteIterator, ByteStringBuilder, ByteString}
 import org.apache.commons.lang3.StringUtils
 
 trait ByteOrderImplicit {
@@ -27,8 +27,10 @@ trait BitcoinMessage extends ByteOrderImplicit {
   }
 }
 
-object BitcoinMessage {
+object BitcoinMessage extends ByteOrderImplicit {
   val magic = 0xD9B4BEF9
+  type Hash = Array[Byte]
+  type Script = Array[Byte]
 
   def calcChecksum(data: ByteString) = {
     val hash = dsha(data.toArray[Byte])
@@ -41,4 +43,57 @@ object BitcoinMessage {
     md.digest()
   }
   val dsha = sha256 compose sha256
+
+  implicit class ByteStringBuilderExt(bb: ByteStringBuilder) {
+    def putVarString(s: String) = {
+      putVarInt(s.length)
+      bb.putBytes(s.getBytes)
+    }
+    def putScript(script: Script) = {
+      putVarInt(script.length)
+      bb.putBytes(script)
+    }
+    def putVarInt(i: Int) = {
+      assert(i >= 0)
+      if (i <= 0xFC)
+        bb.putByte(i.toByte)
+      else if (i <= 0xFFFF) {
+        bb.putByte(0xFD.toByte)
+        bb.putShort(i.toShort)
+      }
+      else {
+        bb.putByte(0xFE.toByte)
+        bb.putInt(i)
+      }
+    }
+  }
+
+  implicit class ByteStringIteratorExt(bi: ByteIterator) {
+    def getVarString: String = {
+      val length = getVarInt
+      val s: Array[Byte] = new Array(length)
+      bi.getBytes(s)
+      new String(s)
+    }
+    def getVarInt: Int = {
+      val b = bi.getByte
+      b match {
+        case -3 => (bi.getShort & 0x0000FFFF)
+        case -2 => (bi.getInt & 0x7FFFFFFF)
+        case -1 => bi.getLong.toInt
+        case _ => (b.toInt & 0x000000FF)
+      }
+    }
+    def getHash: Hash = {
+      val h: Hash = new Array(32)
+      bi.getBytes(h)
+      h
+    }
+    def getScript: Script = {
+      val len = bi.getVarInt
+      val script: Script = new Array(len)
+      bi.getBytes(script)
+      script
+    }
+  }
 }
